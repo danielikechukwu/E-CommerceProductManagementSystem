5e50cdbbca34add968ce7356691f4c361161ed2b
Daniel Okwukwe Ikechukwu <ikechukwudaniel080@gmail.com>
2026-04-13 16:14:08 +0100

    Initial commit

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceProductManagementSystem.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Category name is required")]
+    [StringLength(100, ErrorMessage = "Category name cannot exceed 100 characters")]
+    public string Name { get; set; } = string.Empty;
+    
+    [StringLength(100, ErrorMessage = "Category description cannot exceed 100 characters")]
+    public string? Description { get; set; } = string.Empty;
+    
+    public ICollection<Product>?  Products { get; set; }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_CommerceProductManagementSystem.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Product name is required")]
+    [StringLength(100, ErrorMessage = "Product name cannot exceed 100 characters")]
+    public string Name { get; set; } = string.Empty;
+    
+    [StringLength(100, ErrorMessage = "Product description cannot exceed 100 characters")]
+    public string? Description { get; set; }
+    
+    [Required(ErrorMessage = "Price is required")]
+    [Range(0.01, 999999.99, ErrorMessage = "Price must be between 0.01 and 999999.99")]
+    [Column(TypeName = "decimal(12, 2)")]
+    public decimal Price { get; set; }
+    
+    [ForeignKey(nameof(Category))]
+    [Required(ErrorMessage = "Category is required")]
+    public int CategoryId { get; set; }
+    
+}
